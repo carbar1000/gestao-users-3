5a308fb2564e2d79635a2b642c264c4178b20ff1
carbar1000 <carlosbarradas.cripto@gmail.com>
2025-02-26 12:05:04 +0000

    enviando para o giti

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Configurações do Supabase
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// Acesse as variáveis de ambiente
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Defina a variável no Vercel
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // Defina a variável no Vercel
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
