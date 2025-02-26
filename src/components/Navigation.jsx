0cc0b64cccf5284f7150c06c7b9345e9232e922f
carbar1000 <carlosbarradas.cripto@gmail.com>
2025-02-26 15:51:41 +0000

    enviando para o giti

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,10 +9,11 @@ const Navigation = () => {
       <ul className="nav-list">
 <li className="nav-item"><Link to="/" className="nav-link">Início</Link></li>
 
-        <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
-        <li className="nav-item"><Link to="/add-user" className="nav-link">Add User</Link></li>
-        <li className="nav-item"><Link to="/users-list" className="nav-link">Users List</Link></li>
-        <li className="nav-item"><Link to="/users-status" className="nav-link">Users Status</Link></li>
+        <li className="nav-item"><Link to="/about" className="nav-link">Sobre</Link></li>
+        <li className="nav-item"><Link to="/add-user" className="nav-link">Adicionar Usuário</Link></li>
+        <li className="nav-item"><Link to="/users-list" className="nav-link">Lista de Usuários</Link></li>
+        <li className="nav-item"><Link to="/users-status" className="nav-link">Status dos Usuários</Link></li>
+
       </ul>
     </nav>
   );
